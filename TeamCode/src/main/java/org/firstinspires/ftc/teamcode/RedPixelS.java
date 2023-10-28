d228a063fa6bbfff32b21fdf75081e526af114d5
ftcmuskogee <43355408+ftcmuskogee@users.noreply.github.com>
2023-10-28 16:07:28 -0500

    deadreackon

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedPixelS.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+
+@Autonomous (name = "Red Pixel Side", group = "Autonomous Main")
+public class RedPixelS extends LinearOpMode {
+    @Override
+    public void runOpMode(){
+
+    }
+
+}
+private void
